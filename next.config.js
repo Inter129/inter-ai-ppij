b94c4fdcf9a5a1d369b4212bf425b07c0a6e6f1f
sangho129 <sangho129@icloud.com>
2023-02-23 20:30:15 +0900

    Oein's Support!
    Co-authored-by: SungHyun <Oein@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/chat",
+        destination:
+          "https://builder.pingpong.us/api/builder/pingpong/chat/demo",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { Button, Textarea, Spacer } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 
 export default function App() {
+  let [ppv, setppv] = useState("");
   return (
     <>
       <div
@@ -13,25 +15,48 @@ export default function App() {
           position: "fixed",
         }}
       >
-        <Textarea placeholder="질문" />
+        <Textarea
+          defaultValue={ppv}
+          onChange={(v) => {
+            setppv(v.target.value || ppv || "");
+          }}
+          placeholder="질문"
+        />
         <Spacer y={0.5} />
         <Button
           onClick={() => {
+            console.log(ppv);
             var config = {
               method: "get",
               maxBodyLength: Infinity,
-              url: "/api/req?pp=ㅎㅇ",
-              headers: {},
+              url: `/api/chat?query=${ppv}`,
+              headers: {
+                "sec-ch-ua":
+                  '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+                Accept: "application/json, text/plain, */*",
+                "sec-ch-ua-mobile": "?0",
+                "User-Agent":
+                  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                "sec-ch-ua-platform": '"macOS"',
+                "Sec-Fetch-Site": "same-site",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Dest": "empty",
+                "sec-gpc": "1",
+                host: "builder.pingpong.us",
+              },
             };
+
             axios(config)
               .then(function (response) {
-                var data = JSON.stringify(response.data);
-                data = JSON.parse(data).rp;
-                toast(data);
-                console.log(data);
+                var data = response.data;
+                data = JSON.stringify(data)
+                  .replace("핑퐁이", "인터")
+                  .replace("pingpong.us", "sangho.xyz")
+                  .replace("핑퐁", "인터");
+                toast(JSON.parse(data).response.replies[0].reply);
               })
               .catch(function (error) {
-                console.log(error);
+                toast.error("Error occurred: " + error);
               });
           }}
         >
